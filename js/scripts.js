904fe92fd937b8472166f7c392161a2710280d81
Dave Brown <daveb.za@gmail.com>
2017-06-29 20:51:46 +0800

    FEAT: added a second clickable cat, names and two tallys

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
-var score = 0;
+var score1 = 0;
+var score2 = 0;
 console.log("App initialized. Good to go.");
+var catName1 = "Tiddles";
+var catName2 = "Fluffy";
 
-$('#catPic').click(function(e) {
-  score++;
-  $('#score').text(score);
+$('#catName1').text(catName1);
+$('#catName2').text(catName2);
+
+
+$('#catPic1').click(function(e) {
+  score1++;
+  $('#score1').text(score1);
+});
+
+$('#catPic2').click(function(e) {
+  score2++;
+  $('#score2').text(score2);
 });
