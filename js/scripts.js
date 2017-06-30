2b802f86b6a5a1d4dc89ea08f6fc1fcb226daa35
Dave Brown <daveb.za@gmail.com>
2017-06-30 11:28:20 +0800

    FEAT: completed the catData object

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var catData = {
+    "cat1": {
+        "name": "Tiddles",
+        "clickCount": 0
+    },
+    "cat2": {
+        "name": "Fluffy",
+        "clickCount": 0
+    },
+    "cat3": {
+        "name": "Mouser",
+        "clickCount": 0
+    },
+    "cat4": {
+        "name": "Napoleon",
+        "clickCount": 0
+    },
+    "cat5": {
+        "name": "Osiris",
+        "clickCount": 0
+    },
+};
+
+var listView = [
+
+];
+
+var detailView = [
+
+];
+
+var controller = [
+
+];
