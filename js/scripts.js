6d18ed41a831bcea796cf9d8dcb27e442307a42f
Dave Brown <daveb.za@gmail.com>
2017-06-30 09:44:01 +0800

    FEAT: refactored the method for displaying pic and score to scale for multiple cats

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
-var score1 = 0;
-var score2 = 0;
+var score = 0;
 console.log("App initialized. Good to go.");
 var catName1 = "Tiddles";
 var catName2 = "Fluffy";
 
-$('#catName1').html("<h2>Cat: "+catName1+"</h2>");
+$('#catName').html("<h2>Cat: "+catName1+"</h2>");
 
-$('#catPic1').click(function(e) {
-  score1++;
-  $('#output').text("Now you've clicked this cat "+score1+" times.");
+$('#catPic').click(function(e) {
+  score++;
+  $('#output').text("Now you've clicked this cat "+score+" times.");
 });
 
 
