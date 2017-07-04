8ad3ab78887e6ac3abbc46af8a360d775ce15424
Dave Brown <daveb.za@gmail.com>
2017-07-04 17:46:54 +0800

    FEAT: autopopulate the cat name list

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,45 @@
 var catData = {
     cats:[
         {
-        name: "Tiddles",
-        clickCount: 0,
-        imgsrc:""},
+            name: "Tiddles",
+            clickCount: 0,
+            imgsrc:""
+        },
         {
-        "name": "Fluffy",
-        "clickCount": 0,
-        "imgsrc":""},
+            name: "Fluffy",
+            clickCount: 0,
+            imgsrc:""
+        },
         {
-        "name": "Mouser",
-        "clickCount": 0,
-        "imgsrc":""},
+            name: "Mouser",
+            clickCount: 0,
+            imgsrc:""
+        },
         {
-        "name": "Napoleon",
-        "clickCount": 0,
-        "imgsrc":""},
+            name: "Napoleon",
+            clickCount: 0,
+            imgsrc:""
+        },
         {
-        "name": "Osiris",
-        "clickCount": 0,
-        "imgsrc":""},
+            name: "Osiris",
+            clickCount: 0,
+            imgsrc:""
+        },
     ]
 };
 
-var detailView = [
+var controller = {
+    getCats: function(){
+        return catData.cats;
+    }
+};
 
-];
+var detailView = {
+    listMaker: function(){
+        var catList = controller.getCats();
+        for (var i = 0; i < catList.length; i++){
+            $('#catList').append('<li>'+catList[i].name+'</li>');
+        }
+    }
 
-var controller = [
-
-];
+};
